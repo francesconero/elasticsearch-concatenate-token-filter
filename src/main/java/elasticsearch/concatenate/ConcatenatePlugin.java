527cd62675eea5a7ae05202b871ad4884799c423
Omer Ben Yosef <bomberby@gmail.com>
2016-07-28 15:05:19 +0900

    Es 2.3.3 (#7)
    * support ES 2.3.3

--- src/main/java/elasticsearch/concatenate/ConcatenatePlugin.java
+++ src/main/java/elasticsearch/concatenate/ConcatenatePlugin.java
@@ -1,20 +1,24 @@
-package elasticsearch.concatenate;
+package org.elasticsearch.index.analysis.concatenate;
 
 import java.util.Collection;
+import org.elasticsearch.index.analysis.AnalysisModule;
 
-import org.elasticsearch.common.collect.Lists;
 import org.elasticsearch.common.component.LifecycleComponent;
 import org.elasticsearch.common.inject.Module;
-import org.elasticsearch.plugins.AbstractPlugin;
+import org.elasticsearch.plugins.Plugin;
 
-public class ConcatenatePlugin extends AbstractPlugin {
+public class ConcatenatePlugin extends Plugin {
     @Override
     public String name() {
-        return "ConcatenatePlugin";
+        return "analysis-concatenate";
     }
 
     @Override
     public String description() {
         return "Plugin that provides a Token Filter that recombines all of the tokens in a token stream back into one.";
+    }
+
+    public void onModule(AnalysisModule module) {
+        module.addTokenFilter("concatenate", ConcatenateTokenFilterFactory.class);
     }
 }
